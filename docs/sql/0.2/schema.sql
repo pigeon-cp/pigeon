62932ab8bbeb3eb9158a4aaa757c756e97843a09
taccisum <liaojinfeng@deepexi.com>
2022-02-11 17:21:06 +0800

    chore: adapt data object adjustments in 'pigeon-core'

--- docs/sql/0.2/schema.sql
+++ docs/sql/0.2/schema.sql
@@ -1,21 +1,28 @@
 ALTER TABLE `message`
     ADD COLUMN sub_mass_id BIGINT NULL;
+ALTER TABLE `message`
+    ADD COLUMN signature VARCHAR(50) NULL AFTER `content`;
+ALTER TABLE `message`
+    ADD COLUMN third_template_code VARCHAR(50) NULL AFTER `template_id`;
+ALTER TABLE `message`
+    ADD COLUMN delivery_id VARCHAR(64) NULL AFTER `params`;
 
 ALTER TABLE `mass_tactic`
     ADD COLUMN source_size INT NULL AFTER `source_type`;
 
 create table sub_mass
 (
-    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    main_id    BIGINT NOT NULL,
-    status     ENUM (
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    main_id     BIGINT      NOT NULL,
+    status      ENUM (
         'INIT',
         'DELIVERING',
         'DELIVERED'
         ),
-    serial_num INT    NOT NULL,
-    size       INT    NOT NULL,
-    start      INT    NOT NULL
+    delivery_id VARCHAR(64) NULL,
+    serial_num  INT         NOT NULL,
+    size        INT         NOT NULL,
+    start       INT         NOT NULL
 );
 
 ALTER TABLE `message_template`
@@ -23,3 +30,7 @@ ALTER TABLE `message_template`
 
 ALTER TABLE `message_mass`
     ADD COLUMN type VARCHAR(50) AFTER id;
+ALTER TABLE `message_mass`
+    ADD COLUMN message_type VARCHAR(50) AFTER `type`;
+ALTER TABLE `message_mass`
+    ADD COLUMN sp_type VARCHAR(50) AFTER `message_type`;
