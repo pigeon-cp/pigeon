7616bf242f1da20a2fbfeeb4cc6672d0c8c06d52
taccisum <liaojinfeng@deepexi.com>
2022-02-22 12:34:54 +0800

    fix: make params support pass obj & add fields docs

--- src/main/java/com/github/taccisum/pigeon/dto/SendTemplateMessageRequest.java
+++ src/main/java/com/github/taccisum/pigeon/dto/SendTemplateMessageRequest.java
@@ -12,15 +12,15 @@ import java.util.Map;
  */
 @Data
 public class SendTemplateMessageRequest {
-    @ApiModelProperty
+    @ApiModelProperty(value = "消息发送者")
     private String sender;
     @NotNull
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(value = "消息目标对象，以 u_ 开头表示其是一个用户", required = true)
     private String target;
-    @ApiModelProperty
-    private String params;
-    @ApiModelProperty
+    @ApiModelProperty("模板参数，String | Object")
+    private Object params;
+    @ApiModelProperty("消息签名")
     private String signature;
-    @ApiModelProperty
+    @ApiModelProperty("消息扩展参数，视具体消息类型不同而不同")
     private Map<String, Object> ext;
 }
